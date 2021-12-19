89c7f278e5ead8ee27a268ad0104ec455f617300
tghsd <90035059+tghsd@users.noreply.github.com>
2021-12-19 15:39:30 +0300

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -1,7 +1,7 @@
 package other;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Problem {
@@ -9,16 +9,76 @@ public class Problem {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        List<Visitor> visitors = new ArrayList<>();
+        ArrayList<Node> nodes = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            visitors.add(new Visitor(i, timeToMin(in.next()), timeToMin(in.next())));
+            nodes.add(new Node(i, timeToMin(in.next()), 1));
+            nodes.add(new Node(i, timeToMin(in.next()), -1));
         }
 
-        
+        Collections.sort(nodes);
+
+        for (Node i : nodes) {
+            System.out.println(i);
+        }
+
+        int cnt = 0;
+        int maxCnt = 0;
+        int left = -1, right = -1;
+        for (int i = 0; i < nodes.size(); i++) {
+            cnt += nodes.get(i).getCome();
+            if (cnt > maxCnt) {
+                maxCnt = cnt;
+                left = nodes.get(i).getNum();
+            } else {
+                right = nodes.get(i - 1).getNum();
+            }
+        }
+
+        System.out.println(left + " " + right);
+
     }
 
     private static int timeToMin(String s) {
         return Integer.parseInt(s.substring(0, 2)) * 60 + Integer.parseInt(s.substring(3, 5));
+    }
+}
+
+class Node implements Comparable {
+    private final int num;
+    private final int time;
+    private final int come;
+
+    public Node(int num, int time, int come) {
+        this.num = num;
+        this.time = time;
+        this.come = come;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getCome() {
+        return come;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + num + ", " + time + ", " + come + ")";
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Node obj = (Node) o;
+        int ans = Integer.compare(time, obj.getTime());
+        if (ans == 0) {
+            ans = Integer.compare(-come, -obj.getCome());
+        }
+        return ans;
     }
 }
 
