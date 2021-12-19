784a03376cc7c85120a9c55658a7a427660a9c81
tghsd <90035059+tghsd@users.noreply.github.com>
2021-12-19 17:09:35 +0300

    Update NewProblem.java

--- NewProblem.java
+++ NewProblem.java
@@ -1,28 +1,57 @@
+package other;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class NewProblem {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+public class Problem {
+    private static Scanner in;
+    private static final String INF = "20000000000";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        in = new Scanner(new FileReader(
+                "C:\\Users\\User\\IdeaProjects\\untitled12\\src\\other\\fin.txt"
+        ));
         int n = in.nextInt();
+
 
         ArrayList<Node> nodes = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            nodes.add(new Node(in.next(), 1));
-            nodes.add(new Node(in.next(), -1));
+            String timeIn = nextTime();
+            String timeOut = nextTime();
+            if (timeIn.compareTo("1633305600") >= 0 && timeOut.compareTo("1633305600") >= 0) {
+                nodes.add(new Node(timeIn, 1));
+                nodes.add(new Node(timeOut, -1));
+            }
         }
 
         Collections.sort(nodes);
 
         int cnt = 0;
         int maxCnt = 0;
+        String left = "", right = "";
         for (int i = 0; i < nodes.size(); i++) {
             cnt += nodes.get(i).getCome();
-            maxCnt = Math.max(maxCnt, cnt);
+            if (cnt > maxCnt) {
+                left = nodes.get(i).getTime();
+                right = nodes.get(i + 1).getTime();
+                maxCnt = cnt;
+            }
         }
 
         System.out.println(maxCnt);
+        System.out.println(left + " " + right);
+        System.out.println(Long.parseLong(right) - Long.parseLong(left));
+    }
+
+    private static String nextTime() {
+        String time = in.next();
+        if (time.equals("0")) {
+            return INF;
+        }
+        return time;
     }
 }
 
